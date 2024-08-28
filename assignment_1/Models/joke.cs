378f13e5b0b3ff7075017ef75252f373a8cdf7e0
naibemt <naibemt@uia.no>
2024-08-28 12:57:15 +0200

    assignment_1/

---
+++ assignment_1/Models/joke.cs
@@ -0,0 +1,14 @@
+namespace assignment_1.Models;
+
+public class joke
+{
+    public int id { get; set; }
+    public string JokeQuestion { get; set; }
+    public string JokeAnswer { get; set; }
+
+    public joke()
+    {
+        
+    }
+    
+}
